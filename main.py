7608202425c0d3a1233631aa8e8738e6c90d6cab
yasenAlexiev <yassen4o222@abv.bg>
2025-01-27 12:29:54 +0200

    some changes that I made after I see the answers of the test

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 def task1():
     st = 'Print only the words that start with s in this sentence'
     for word in st.split():
-        if word[0] == 's':
+        if word[0].lower() == 's':
             print(word)
 
 
 # Use range() to print all the even numbers from 0 to 10.
 def task2():
-    print(list(range(0, 10, 2)))
+    print(list(range(0, 11, 2)))
 
 
 # Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 def task3():
-    print([num for num in range(1, 50) if num % 3 == 0])
+    print([num for num in range(1, 51) if num % 3 == 0])
 
 
 # Go through the string below and if the length of a word is even print "even!"
@@ -28,13 +28,14 @@ def task4():
 # and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 def task5():
     for x in range(1, 101):
-        if x % 3 == 0:
-            print('Fizz')
-            continue
-        if x % 5 == 0:
+        if x % 3 == 0 and x % 5 == 0:
+            print('FizzBuzz')
+        elif x % 3 == 0:
+            print('Fuzz')
+        elif x % 5 == 0:
             print('Buzz')
-            continue
-        print(x)
+        else:
+            print(x)
 
 
 # Use List Comprehension to create a list of the first letters of every word in the string below:
