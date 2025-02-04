4129b4489a1bf004e9a2f71758e48187eb853c25
yasenAlexiev <yassen4o222@abv.bg>
2025-02-04 17:20:04 +0200

    commit my exercises for sending and receiving emails with gmail libraries

---
+++ Emails Exercise/email_receiving.py
@@ -0,0 +1,31 @@
+import imaplib
+import email
+
+M = imaplib.IMAP4_SSL("imap.gmail.com")
+
+email_str = input("Email: ")
+password = input("Password: ")
+
+M.login(email_str, password)
+
+print(M.list())
+print(M.select("inbox"))
+
+typ, data = M.search(None, 'SUBJECT "Special email for testing"')
+
+print(typ)
+print(data)
+
+email_id = data[0]
+result, email_data = M.fetch(email_id, '(RFC822)')
+print(email_data)
+raw_email = email_data[0][1]
+raw_email_string = raw_email.decode('utf-8')
+
+
+email_message = email.message_from_string(raw_email_string)
+
+for part in email_message.walk():
+    if part.get_content_type() == 'text/plain':
+        body = part.get_payload(decode=True)
+        print(body)
---
+++ Emails Exercise/emails_sending.py
@@ -0,0 +1,20 @@
+import smtplib
+import getpass
+
+smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
+print(smtp_object.ehlo())
+print(smtp_object.starttls())
+
+email = input("Email: ")
+password = input("Password please: ")
+smtp_object.login(email, password)
+
+from_email = email
+to_email = input("Email to send a message: ")
+
+subject = input("Enter the subject line:")
+message = input("Enter the message:")
+msg = "Subject: " + subject + "\n" + message
+
+smtp_object.sendmail(from_email, to_email, msg)
+smtp_object.quit()
