6179d7546a9897692f0be96e1685ede3798888a4
yasenAlexiev <yassen4o222@abv.bg>
2025-02-20 11:46:54 +0200

    use nametofont to configure size property of the font of a style

--- TkinterProjects/stylesWithTkinter.py
+++ TkinterProjects/stylesWithTkinter.py
@@ -1,17 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
-
+import tkinter.font as font
 
 root = tk.Tk()
 style = ttk.Style(root)
 
-style.theme_use("clam")
+font.nametofont("TkDefaultFont").configure(size=15)
+font.nametofont("TkTextFont").configure(size=15)
 
-style.map("CustomButton.TButton",
-          foreground=[("pressed", "red"), ("active", "white")],
-          background=[("pressed", "!disabled", "black"), ("active", "black")],
-          font=[("pressed", ("TkDefaultFont", 15))]
-          )
 
 name = ttk.Label(root, text="Hello world!!!")
 entry = ttk.Entry(root, width=15)
