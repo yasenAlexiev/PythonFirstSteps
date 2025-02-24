deed4497e3391661b0a39b38a844d5bf3a13aef6
yasenAlexiev <yassen4o222@abv.bg>
2025-02-24 16:52:11 +0200

    commit settings functionality and redesign the app in different modules and files

---
+++ TkinterProjects/PomodoroTimer/frames/__init__.py
@@ -0,0 +1,2 @@
+from frames.pomodoroTimer import Timer
+from frames.settings import Settings
--- TkinterProjects/PomodoroTimer/frames/pomodoroTimer.py
+++ TkinterProjects/PomodoroTimer/frames/pomodoroTimer.py
@@ -3,44 +3,34 @@ from tkinter import ttk
 from collections import deque
 
 
-class PomodoroTimer(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.title("Pomodoro timer")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-
-        container = ttk.Frame(self)
-        container.grid()
-        container.columnconfigure(0, weight=1)
-
-        timer_frame = Timer(container)
-        timer_frame.grid(row=0, column=0, sticky="NSEW")
-
-
 class Timer(ttk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, controller, show_settings):
         super().__init__(parent)
 
-        self.current_time = tk.StringVar(value="00:10")
-        self.timer_order = ["Pomodoro", "Short Break", "Pomodoro", "Short Break", "Pomodoro", "Long Break"]
-        self.timer_schedule = deque(self.timer_order)
-        self.current_timer_label = tk.StringVar(value=self.timer_schedule[0])
-        self.timer_running = True
+        self.controller = controller
+        pomodoro_time = int(controller.pomodoro.get())
+        self.current_time = tk.StringVar(value=f"{pomodoro_time:02d}:00")
+        self.current_timer_label = tk.StringVar(value=controller.timer_schedule[0])
+        self.timer_running = False
         self._timer_decrement_job = None
 
         timer_description = ttk.Label(self, textvariable=self.current_timer_label)
         timer_description.grid(row=0, column=0, sticky="W", padx=(10, 0), pady=(10, 0))
 
+        settings_button = ttk.Button(self,
+                                     text="Settings",
+                                     command=show_settings,
+                                     cursor="hand2")
+        settings_button.grid(row=0, column=1, sticky="E", padx=10, pady=10)
+
         timer_frame = ttk.Frame(self, height="100")
-        timer_frame.grid(row=1, column=0, pady=(10, 0), sticky="NSEW")
+        timer_frame.grid(row=1, column=0, columnspan=2, pady=(10, 0), sticky="NSEW")
 
         timer_counter = ttk.Label(timer_frame, textvariable=self.current_time)
         timer_counter.place(relx=0.5, rely=0.5, anchor="center")
 
         button_container = ttk.Frame(self, padding=10)
-        button_container.grid(row=2, column=0, sticky="EW")
+        button_container.grid(row=2, column=0, columnspan=2, sticky="EW")
         button_container.columnconfigure((0, 1, 2), weight=1)
 
         self.start_button = ttk.Button(
@@ -85,9 +75,10 @@ class Timer(ttk.Frame):
 
     def reset_timer(self):
         self.stop_timer()
-        self.current_time.set("25:00")
-        self.timer_schedule = deque(self.timer_order)
-        self.current_timer_label.set(self.timer_schedule[0])
+        pomodoro_time = int(self.controller.pomodoro.get())
+        self.current_time.set(f"{pomodoro_time:02d}:00")
+        self.controller.timer_schedule = deque(self.controller.timer_order)
+        self.current_timer_label.set(self.controller.timer_schedule[0])
 
     def decrement_time(self):
         current_time = self.current_time.get()
@@ -105,19 +96,18 @@ class Timer(ttk.Frame):
             self.current_time.set(f"{minutes:02d}:{seconds:02d}")
             self._timer_decrement_job = self.after(1000, self.decrement_time)
         elif self.timer_running and current_time == "00:00":
-            self.timer_schedule.rotate(-1)
-            next_up = self.timer_schedule[0]
+            self.controller.timer_schedule.rotate(-1)
+            next_up = self.controller.timer_schedule[0]
             self.current_timer_label.set(next_up)
 
             if next_up == "Pomodoro":
-                self.current_time.set("25:00")
+                pomodoro_time = int(self.controller.pomodoro.get())
+                self.current_time.set(f"{pomodoro_time:02d}:00")
             elif next_up == "Short Break":
-                self.current_time.set("05:00")
+                short_break_time = int(self.controller.short_break.get())
+                self.current_time.set(f"{short_break_time:02d}:00")
             elif next_up == "Long Break":
-                self.current_time.set("15:00")
+                long_break_time = int(self.controller.long_break.get())
+                self.current_time.set(f"{long_break_time:02d}:00")
 
             self._timer_decrement_job = self.after(1000, self.decrement_time)
-
-
-app = PomodoroTimer()
-app.mainloop()
